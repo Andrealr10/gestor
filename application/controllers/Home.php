323211d5416ecd416acea20e124016f890a7e890
Victoria Castro <victoria.akicastro92@gmail.com>
2019-08-21 14:39:47 -0600

    Vista de navbar

---
+++ application/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+		// $this->load->library('javascript'); esto ya no tiene soporte
+		// $this->load->helper('url'); // Se utiliza un helper para manejo de url
+		$this->load->view('includes/nav'); // Menu
+		$this->load->view('home'); 
+		$this->load->view('includes/foot');
+	}
+}
