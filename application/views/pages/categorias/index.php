f2612a1919961b3d96fe0a39cd9807ff0c14ec1d
Rod-E1996 <rjgespinoza96@gmail.com>
2019-09-05 11:52:21 -0600

    Modales añadidos a categorias y sub

--- application/views/pages/categorias/index.php
+++ application/views/pages/categorias/index.php
@@ -17,34 +17,66 @@
 <body>
     <div class="container-fluid content-container filter-container">
         <div class="row">
-            <div class="col-sm-2">
-                <div id="add">
-                    <form id="form" enctype="multipart/form-data">
-                        <label for="nombre">Nombre de la Categoria</label><br>
-                        <input type="text" name="nombre" id="nombre"><br>
-                        <label for="descripcion">Descripcion de la Categoria</label><br>
-                        <textarea name="descripcion" id="descripcion" cols="30" rows="10"></textarea><br>
-
-
-                        <label for="imagen">Imagen de la Categoria</label><br>
-                        <input type="file" name="imagen" accept=".png,.jpg,.jpeg" id="imagen"><br>
-                        <label for="algo">Foto:</label>
-                        <img src="" id="algo" alt="Sin Imagen" style="width: 200px; height: 200px">
-                        <br><br>
-                        <button id="crear" type="button">Crear</button>
-                    </form>
-                </div>
-            </div>
-            <div class="col-sm-10 text-center filtered-cards">
+            <div class="col-sm-11 text-center filtered-cards">
                 <div id="card-placeholder">
                     <div id="cards"></div>
-
+                    
                 </div>
-
+            </div>
+            <div class="col-sm-1">
+                <!-- Botón que despliega el modal -->
+                <a class="botton circle" style="color:white;" data-toggle="modal" data-target="#modelId">Crear <i class="fas fa-plus-square fa-lg"></i></a>
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Nueva Categoría</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                </div>
+                <div class="modal-body">
+                    <div id="add">
+                            <form id="form" enctype="multipart/form-data">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1"><i class="fas fa-pen"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Título de la categoría" aria-describedby="basic-addon1">
+                                </div>
+
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon2"><i class="fas fa-bookmark"></i></span>
+                                    </div>
+                                    <textarea name="descripcion" class="form-control" placeholder="Descripción" id="descripcion" cols="30" rows="3" aria-describedby="basic-addon2"></textarea>
+                                </div> 
+    
+                                <input type="file" style="display:none;" name="imagen" accept=".png,.jpg,.jpeg" id="imagen">
+                                <input type="button" class="btn btn-primary" value="Cargar imágen" onclick="document.getElementById('imagen').click();"><br>
+                                <!-- <label for="algo">Foto:</label> -->
+                                <img class="mt-3" src="" id="algo"  style="width: 200px; height: 200px">
+                                <br><br>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                    <button id="crear" class="btn btn-danger" data-dismiss="modal" type="button">Crear</button>
+                    <button type="button" id="limpiar" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
     <script src="<?= base_url(); ?>libs/js/jquery-3.4.1.min.js"></script>
+    <script src="<?= base_url(); ?>libs/js/popper.min.js"></script>
     <script src="<?= base_url(); ?>libs/js/bootstrap.min.js"></script>
     <script src="<?= base_url(); ?>libs/js/categorias.js"></script>
 
@@ -57,6 +89,22 @@
                 var TmpPath = URL.createObjectURL(e.target.files[0]);
                 $('#algo').attr('src', TmpPath);
             } 
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#limpiar').click(function() {
+                $('input[type="text"]').val('');
+                $('textarea').val('');
+                jQuery('#algo').attr('src','')
+                jQuery('#algo').show();
+            });
+            $('#crear').click(function() {
+                $('input[type="text"]').val('');
+                $('textarea').val('');
+                jQuery('#algo').attr('src','')
+                jQuery('#algo').show();
+            });
         });
     </script>
 </body>
--- application/views/pages/subcategorias/index.php
+++ application/views/pages/subcategorias/index.php
@@ -16,41 +16,89 @@
     <a href="<?= base_url('categoria') ?>" class="buton"><i class="large material-icons">arrow_back</i></a>
     <div class="container-fluid">
         <div class="row">
-            <?php foreach ($subcategorias as $subcategoria) { ?>
-                <div class="col-sm-6 col-lg-4 mt-5 ">
-                    <div class="frame">
-                        <div class="center">
-                            <div class="card">
-                                <div class="left">
-                                    <div class="avatar">
-                                        <div class="circle"></div>
-                                        <div class="circle"></div>
-                                        <img src="<?= base_url() . '/' . $subcategoria->imagen ?>">
+            <!-- <div class="col-md-6 col-lg-4 mt-5 ">
+                <form id="form">
+                    <label for="nombre">Nombre de la SubCategoria</label><br>
+                    <input type="text" name="nombre" id="nombre"><br>
+                    <input type="text" name="categoria" id="categoria" hidden value="<?= $id_categoria ?>">
+                    <br>
+                    <button type="button" id="crear">Crear</button>
+                </form>
+            </div> -->
+            
+            <div class="col-md-1">
+                <!-- Botón que despliega el modal -->
+                <a class="botton circle" style="color:white;" data-toggle="modal" href="#subcategoria">Crear <i class="fas fa-plus-square fa-lg"></i></a>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="subcategoria" tabindex="-1" role="dialog" aria-labelledby="subcategoriaLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Subcategoria</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span style="position:relative;" aria-hidden="true">&times;</span>
+                                </button>
+                        </div>
+                        <div class="modal-body">
+                            <form id="form">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span style="position:relative;" class="input-group-text" id="basic-addon1"><i class="fas fa-pen"></i></span>
                                     </div>
-                                    <div class="info">
-                                        <span class="big"><?= $subcategoria->nombre ?></span><span class="small"><?= $categoria ?></span>
-                                    </div>
-                                    <div class="botons">
-                                        <button class="flw boton"><a class="link" href="<?= base_url('archivo') ?>/load/<?= $categoria ?>/<?= $subcategoria->nombre ?>/">Ver Contenido</a></button>
-                                    </div>
+                                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="nombre de la subcategoria" aria-label="nombre" aria-describedby="basic-addon1">
+                                    <input type="text" name="categoria" id="categoria" hidden value="<?= $id_categoria ?>">
                                 </div>
-                                <div class="right">
-                                    <div class="stats posts">
-                                        <span class="big"><?= $subcategoria->cantidad ?></span><span class="small">Archivos</span>
-                                    </div>
-                                    <div data-toggle="modal" data-target="#share<?= $subcategoria->id_subcategoria ?>" class="stats likes" style="background-color:rgb(66, 66, 66);" onmouseover="this.style.backgroundColor='gray'" onmouseout="this.style.backgroundColor='rgb(66, 66, 66)'">
+                                
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <!-- <button id="crear" type="button" class="btn btn-secondary" data-dismiss="modal">Crear</button> -->
+                            <button type="button" id="crear" class="btn btn-danger" data-dismiss="modal">Crear</button>
+                            <button type="button" id="limpiar" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <?php foreach ($subcategorias as $subcategoria) { ?>
+                
+                
+            <div class="col-md-3 mt-5 ">
+                <div class="frame">
+                    <div class="center">
+                        <div class="card">
+                            <div class="left">
+                                <div class="avatar">
+                                    <div class="circle"></div>
+                                    <div class="circle"></div>
+                                    <img src="<?= base_url() . '/' . $subcategoria->imagen ?>">
+                                </div>
+                                <div class="info">
+                                    <span class="big"><?= $subcategoria->nombre ?></span><span class="small"><?= $categoria ?></span>
+                                </div>
+                                <div class="botons">
+                                    <button class="flw boton"><a class="link" href="<?= base_url('archivo') ?>/load/<?= $categoria ?>/<?= $subcategoria->nombre ?>/">Ver Contenido</a></button>
+                                </div>
+                            </div>
+                            <div class="right">
+                                <div class="stats posts">
+                                    <span class="big"><?= $subcategoria->cantidad ?></span><span class="small">Archivos</span>
+                                </div>
+                                <div data-toggle="modal" data-target="#share<?= $subcategoria->id_subcategoria ?>" class="stats likes" style="background-color:rgb(66, 66, 66);" onmouseover="this.style.backgroundColor='gray'" onmouseout="this.style.backgroundColor='rgb(66, 66, 66)'">
                                         <span class="big"><i class="fas fa-user-friends"></i></span><span class="small">Compartir</span>
                                     </div>
-                                    <div class="stats flwrs">
-                                        <a href="<?= base_url('zip') ?>/dir/<?= $categoria ?>/<?= $subcategoria->nombre ?>">
-                                            <span class="big"><i class="fas fa-cloud-download-alt"></i></span><span class="small">Descargar</span>
-                                        </a>
-                                    </div>
+                                <div class="stats flwrs">
+                                    <a href="<?= base_url('zip') ?>/dir/<?= $categoria ?>/<?= $subcategoria->nombre ?>">
+                                    <span class="big"><i class="fas fa-cloud-download-alt"></i></span><span class="small">Descargar</span>
+                                </a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
                 <div class="modal fade" id="share<?= $subcategoria->id_subcategoria ?>" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
                     <div class="modal-dialog modal-sm" role="document">
                         <div class="modal-content">
@@ -68,22 +116,31 @@
                         </div>
                     </div>
                 </div>
-            <?php } ?>
-            <div class="col-sm-6 col-lg-4 mt-5 ">
-                <form id="form">
-                    <label for="nombre">Nombre de la SubCategoria</label><br>
-                    <input type="text" name="nombre" id="nombre"><br>
-                    <input type="text" name="categoria" id="categoria" hidden value="<?= $id_categoria ?>">
-                    <br>
-                    <button type="button" id="crear">Crear</button>
-                </form>
+                <?php } ?>
             </div>
         </div>
     </div>
 
     <script src="<?= base_url(); ?>libs/js/jquery-3.4.1.min.js"></script>
+    <script src="<?= base_url(); ?>libs/js/popper.min.js"></script>
     <script src="<?= base_url(); ?>libs/js/bootstrap.min.js"></script>
     <script src="<?= base_url(); ?>libs/js/subcategorias.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#limpiar').click(function() {
+                $('input[type="text"]').val('');
+                $('textarea').val('');
+                jQuery('#algo').attr('src','')
+                jQuery('#algo').show();
+            });
+            $('#crear').click(function() {
+                $('input[type="text"]').val('');
+                $('textarea').val('');
+                jQuery('#algo').attr('src','')
+                jQuery('#algo').show();
+            });
+        });
+    </script>
 </body>
 
 </html>
