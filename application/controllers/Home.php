1ab7aa84dfd3d91e0ac147e15c9d9a319357fa7c
Victoria Castro <victoria.akicastro92@gmail.com>
2019-09-06 16:23:19 -0600

    Nueva navbar usuario

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends CI_Controller {
 	{
 		// $this->load->library('javascript'); esto ya no tiene soporte
 		// $this->load->helper('url'); // Se utiliza un helper para manejo de url
-		$this->load->view('includes/nav'); // Menu
+		$this->load->view('includes/navUser'); // Menu
 		$this->load->view('home'); 
 		$this->load->view('includes/foot');
 	}
