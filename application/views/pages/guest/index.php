d33abb971cc11279f509cad6caf0403778e0408f
Rod-E1996 <rjgespinoza96@gmail.com>
2019-09-10 14:29:25 -0600

    vista visitantes casi finalizada

--- application/views/pages/guest/index.php
+++ application/views/pages/guest/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="<?= base_url(); ?>libs/fonts/css/all.css">
     <script src="<?= base_url(); ?>libs/js/jquery-3.4.1.min.js"></script>
     <script src="<?= base_url(); ?>libs/js/popper.min.js"></script>
-    <script src="<?= base_url(); ?>libs/js/bootstrap.min.js"></script>
+    <script src="<?= base_url(); ?>libs/js/bootstrap.js"></script>
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
     <title>Document</title>
@@ -17,7 +17,7 @@
 <body>
     <header class="header">
         <nav class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="#"><img width="50px" src="<?= base_url(); ?>libs/images/logo.jpg"></a>
+            <a class="navbar-brand" href="#"><img width="50px" src="<?= base_url(); ?>libs/images/cat.png"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -46,10 +46,10 @@
                         </div>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link botonNavbar pulse sign">Iniciar Sesión <i class="fas fa-sign-in-alt"></i></a>
+                        <a class="nav-link botonNavbar pulse sign">Sign In <i class="fas fa-sign-in-alt"></i></a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link botonNavbar pulse sign">Regístrate! <i class="fas fa-user-plus"></i></a>
+                        <a class="nav-link botonNavbar pulse sign" style="color:#71ad37;">Registrate <i class="fas fa-user-plus"></i></a>
                     </li>
                 </ul>
             </div>
@@ -58,7 +58,7 @@
 
     <div class="container-fluid mt-4 mb-4">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-12 col-lg-4">
                 <h2>¿Que es CloudCat?</h2>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatibus cum blanditiis omnis deserunt, laudantium corporis laborum eveniet quia quos recusandae minima ea, quidem mollitia. Accusamus cupiditate voluptate libero facilis!
                 Quibusdam facilis tenetur modi sunt soluta, nulla quis tempora quo porro suscipit dicta enim corrupti error pariatur, ullam consequuntur nostrum. Aperiam consectetur maxime expedita dolore. Doloremque sequi non libero temporibus?
@@ -80,16 +80,16 @@
             </div> -->
 
             <!-- *********************Carousel*********************** -->
-            <div class="col-md-8">
+            <!-- <div class="col-md-12 col-lg-8">
                 <div class="csslider infinity" id="slider1">
-                    <input type="radio" name="slides" id="slides_1"/>
-                    <input type="radio" name="slides" checked="checked" id="slides_2"/>
+                    <input type="radio" name="slides" checked="checked" id="slides_1"/>
+                    <input type="radio" name="slides" id="slides_2"/>
                     <input type="radio" name="slides" id="slides_3"/>
                     <input type="radio" name="slides" id="slides_4"/>
                     <input type="radio" name="slides" id="slides_5"/>
                     <input type="radio" name="slides" id="slides_6"/>
                     <ul class="carousel_ul">
-                        <li>
+                        <li style="text-align:center;">
                             <h1>CloudCat <i class="fas fa-paw"></i></h1>
                             <p>La Mejor opción para guardar y compartir de forma segura tus archivos y documentos</p>
                         </li>
@@ -100,14 +100,14 @@
                                 <div>
                                     <div>
                                         <h1>¿Como Surgió?</h1>
-                                        <p>When slide 3 is reached - play CSS animation!</p>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus deserunt alias repudiandae aliquam aut amet asperiores delectus pariatur odio officia ipsum voluptatum ad, quidem quo, ratione repellat earum! Minima, earum?</p>
                                     </div>
                                 </div>
                             </div>
                         </li>
                         <li class="scrollable">
-                            <h1>Lots of text</h1>
-                            <h2>Scrollable one</h2>
+                            <h1>¿Como funciona CloudCat? <i class="fas fa-paw"></i></h1>
+                            <h2 class="subtitulos_carousel">Sus funciones y atributos</h2>
                             <p>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada mi,
                                 semper id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.
@@ -124,7 +124,7 @@
                             </p>
                         </li>
                         <li>
-                            <div id="center"> <a class="nice-link" href="https://github.com/drygiel/csslider" data-text="More examples on github" target="_blank">More examples on github</a></div>
+                            <div id="center"><a class="nice-link" href="#">Acerca del equipo de desarrollo</a></div>
                         </li>
                         <li>
                             <div id="center"><a class="play" id="dex-sign" href="http://drygiel.com" target="_blank"></a></div>
@@ -151,103 +151,133 @@
                         </div>
                     </div>
                 </div><a href="#" target="_blank"></a>
+            </div> -->
+            <div class="col-lg-8">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active slide1">
+                            <h2 class="line anim-typewriter">¿Como usar CloudCat? <i class="fas fa-paw"></i></h2>
+                        </div>
+                        <div class="carousel-item slide2">
+                            <div class="container mt-5">
+                                <h3 style="text-align:left; color:#000;">Crear cuenta CloudCat</h3>
+                                <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis magni fugiat minima voluptate similique dolorum exercitationem cum assumenda id qui ratione dolor reiciendis aperiam, laudantium optio, facilis quidem corporis? Tempore.</p><br>
+                                <h3 style="text-align:left; color:#000;">Iniciar Sesion</h3>
+                                <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis magni fugiat minima voluptate similique dolorum exercitationem cum assumenda id qui ratione dolor reiciendis aperiam, laudantium optio, facilis quidem corporis? Tempore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum debitis ullam dolore dolor blanditiis aut nihil, at earum nobis sapiente, fugit modi reiciendis eos iure. Dolore sed minima soluta magni.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item slide">
+                            <div class="container mt-5">
+
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
 
     <footer class="footer">
-        <div class="footer__addr">
-            <h1 class="footer__logo">CloudCat <i class="fas fa-paw"></i></h1>
-                
-            <h2>Contactanos</h2>
-            
-            <address>
-                &nbsp;<i class="fas fa-phone-alt"></i>  Av. Ricardo Milos, casa #4, Santa Ana<br>
-            </address>
-            <a class="btn sugerencias" type="button" href="#">@ Sugerencias</a>
-        </div>
-
-        <ul class="footer__nav">
-            <li class="nav__item">
-            <h2 class="nav__title">Media</h2>
-            <hr class="subrayado mb-4 mt-0 d-inline-block" style="width: 60px;">
-
-            <ul class="nav__ul">
-                <li>
-                <a href="#">Online</a>
-                </li>
-
-                <li>
-                <a href="#">Print</a>
-                </li>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 footer__addr">
+                    <h1 class="footer__logo">CloudCat <i class="fas fa-paw"></i></h1>
+                        
+                    <h2>Contactanos</h2>
                     
-                <li>
-                <a href="#">Alternative Ads</a>
-                </li>
-            </ul>
-            </li>
-            
-            <li class="nav__item nav__item--extra">
-            <h2 class="nav__title">Technology</h2>
-            <hr class="subrayado mb-4 mt-0 d-inline-block" style="width: 60px;">
-            
-            <ul class="nav__ul nav__ul--extra">
-                <li>
-                <a href="#">Hardware Design</a>
-                </li>
-                
-                <li>
-                <a href="#">Software Design</a>
-                </li>
-                
-                <li>
-                <a href="#">Digital Signage</a>
-                </li>
-                
-                <li>
-                <a href="#">Automation</a>
-                </li>
-                
-                <li>
-                <a href="#">Artificial Intelligence</a>
-                </li>
-                
-                <li>
-                <a href="#">IoT</a>
-                </li>
-            </ul>
-            </li>
-            
-            <li class="nav__item">
-            <h2 class="nav__title">Legal</h2>
-            <hr class="subrayado mb-4 mt-0 d-inline-block" style="width: 60px;">
-            
-            <ul class="nav__ul">
-                <li>
-                <a href="#">Privacy Policy</a>
-                </li>
-                
-                <li>
-                <a href="#">Terms of Use</a>
-                </li>
-                
-                <li>
-                <a href="#">Sitemap</a>
-                </li>
-            </ul>
-            </li>
-        </ul>
+                    <address>
+                        &nbsp;<i class="fas fa-phone-alt"></i>  Av. Ricardo Milos, casa #4, Santa Ana<br>
+                        <a class="btn sugerencias" type="button" href="#">@ Sugerencias</a>
+                    </address>
+                </div>
 
-        <div class="legal">
-            <p>&copy; 2019 CloudCat. All Right Reserved</p>
-            
-            <span class="legal__links">
-            <a href="#">Privacy Policy</a>
-            
-            <span></span>
-            
-            <a href="#">Terms of Use</a>
-            </span>
+                <div class="col-md-8">
+                    <ul class="footer__nav">
+                        <li class="nav__item">
+                            <h2 class="nav__title">Comentanos</h2>
+                            <hr class="subrayado mb-4 mt-0 d-inline-block" style="width: 60px;">
+                
+                            <ul class="nav__ul">
+                                <li>
+                                    <a href="#">Online</a>
+                                </li>
+                
+                                <li>
+                                    <a href="#">Print</a>
+                                </li>
+                                    
+                                <li>
+                                    <a href="#">Alternative Ads</a>
+                                </li>
+                            </ul>
+                        </li>
+                        
+                        <li class="nav__item"> <!--nav__item--extra-->
+                            <h2 class="nav__title">Ayuda</h2>
+                            <hr class="subrayado mb-4 mt-0 d-inline-block" style="width: 60px;">
+                            
+                            <ul class="nav__ul">  <!--nav__ul--extra-->
+                                <li>
+                                    <a href="#">¿Olvidaste tu contraseña?</a>
+                                </li>
+                                
+                                <li>
+                                    <a href="#">Recuperar Usuarios</a>
+                                </li>
+                                
+                                <li>
+                                    <a href="#">No puedo iniciar sesion con mi usuario</a>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                        
+                        <li class="nav__item">
+                            <!-- <h2 class="nav__title">Legal</h2>
+                            <hr class="subrayado mb-4 mt-0 d-inline-block" style="width: 60px;"> -->
+                            
+                            <ul class="nav__ul pl-3">
+                                <div class="hover">
+                                    <span>Redes Sociales</span>
+                                    <a class="social-link" href="#" target="_blank"><i class="fas fa-phone-alt"></i></a>
+                                    <a class="social-link" href="#" target="_blank"><i class="fas fa-envelope"></i></a>
+                                    <a class="social-link" href="#" target="_blank"><i class="fab fa-github"></i></a>
+                                    <a class="social-link" href="#" target="_blank"><i class="fas fa-cog"></i></a>
+                                </div>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+        
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="legal">
+                        <p style="margin-bottom:0;">&copy; 2019 CloudCat. All Right Reserved</p>
+                        
+                        <span class="legal__links">
+                        <a href="#">Privacy Policy</a>
+                        
+                        <span></span>
+                        
+                        <a href="#">Terms of Use</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     </footer>
 </body>
