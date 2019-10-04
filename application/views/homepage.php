f4da3b26afce645855fa36ff90d5d6938420331f
Victoria Castro <victoria.akicastro92@gmail.com>
2019-10-04 13:27:15 -0600

    Pagina de inicio

---
+++ application/views/homepage.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <!-- ----------------------------------Pezonalizado-------------------------------- -->
+     <link rel="stylesheet" href="<?= base_url(); ?>libs/css/homepage.css">
+     <link rel="stylesheet" href="<?= base_url() ?>libs/fonts/css/all.css">
+    <link rel="stylesheet" href="<?= base_url() ?>libs/css/bootstrap.min.css">
+    <title>Cloudcat</title>
+</head>
+<body>
+<header>
+    <div class="header_logo">
+        <!-- <img src="<?= base_url(); ?>libs/images/blackcat.png" "> -->
+        <h1 class="display-4 text-light text-center"> Cloudcat</h1>
+        
+    </div>
+    <div>
+    <nav class="clearfix">
+      <ul class="clearfix topnav snip1555">
+        <li><a href="#">ACERCA DE</a></il>
+        <li><a href="#">TECNOLOGIAS</a></il>
+        <li><a href="#">DESAROLLADORES</a></il>
+        <li><a href="#">REGISTRO &#124; INICIO DE SESI&Oacute;N</a></il>  
+      </ul>
+      <a href="#" id="pull">&#9776;</a>        
+    </nav>
+  </div>
+</header>
+
+  <div class="main">
+     <p>Bacon ipsum dolor amet pork chop filet mignon jerky kielbasa. Tri-tip cow boudin shank flank, venison meatloaf capicola beef doner kielbasa rump. Pastrami meatloaf chicken, spare ribs cupim pork tri-tip. Pancetta beef ribs alcatra, filet mignon tri-tip tongue bacon. Kevin meatloaf tri-tip spare ribs sausage, frankfurter t-bone ham cow pastrami shankle. Chicken turducken corned beef bresaola porchetta kevin ribeye drumstick cupim kielbasa pork belly jerky biltong hamburger.</p> 
+  </div>
+  <script src="<?= base_url() ?>libs/js/jquery-3.4.1.min.js"></script>
+  <!-- ----------------------------------Pezonalizado-------------------------------- -->
+  <script src="<?= base_url() ?>libs/js/homepage.js"></script>
+</body>
+</html>
