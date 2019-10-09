16fba1bce8f624b136908e06c0fba0da199ba1ed
Victoria Castro <victoria.akicastro92@gmail.com>
2019-10-09 11:23:52 -0600

    homepage nuevo

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -7,89 +7,189 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <!-- ----------------------------------Pezonalizado-------------------------------- -->
-     <link rel="stylesheet" href="<?= base_url(); ?>libs/css/homepage.css">
-     <link rel="stylesheet" href="<?= base_url(); ?>libs/css/guest.css">
-     <link rel="stylesheet" href="<?= base_url() ?>libs/fonts/css/all.css">
-    <link rel="stylesheet" href="<?= base_url() ?>libs/css/bootstrap.min.css">
-    <title>Cloudcat</title>
+
+      <link rel="stylesheet" href="<?= base_url(); ?>libs/css/homepage/skel.css">
+      <link rel="stylesheet" href="<?= base_url(); ?>libs/css/homepage/style.css">
+      <link rel="stylesheet" href="<?= base_url(); ?>libs/css/homepage/style-xlarge.css">
+
+     <!-- <link rel="stylesheet" href="<?= base_url() ?>libs/fonts/css/all.css"> -->
+    
+    <title>SuDocs</title>
 </head>
-<body>
+<body id="top">
 <header>
-    <div class="header_logo">
-        <!-- <img src="<?= base_url(); ?>libs/images/blackcat.png" "> -->
-        <h1 class="display-4 text-info text-center"> Cloudcat</h1>
-        
-    </div>
-    <div>
-    <nav class="clearfix">
-      <ul class="clearfix topnav snip1555">
-        <li><a href="#">ACERCA DE</a></il>
-        <li><a href="#">TECNOLOGIAS</a></il>
-        <li><a href="#">DESAROLLADORES</a></il>
-        <li><a href="<?= base_url(); ?>Login">REGISTRO &#124; INICIO DE SESI&Oacute;N</a></il>  
-      </ul>
-      <a href="#" id="pull">&#9776;</a>        
-    </nav>
-  </div>
-</header>
+    <!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="#">SuDocs</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="#" class="button special">Iniciar Sesi&oacute;n</a></li>
+					</ul>
+				</nav>
+			</header>
 
-  <div class="main">
-  <div class="container-fluid mt-4 mb-4">
-        <div class="row">
-            <div class="col-md-12 col-lg-4" id="por_defecto">
-                <h2>¿Que es CloudCat?</h2>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptatibus cum blanditiis omnis deserunt, laudantium corporis laborum eveniet quia quos recusandae minima ea, quidem mollitia. Accusamus cupiditate voluptate libero facilis!
-                Quibusdam facilis tenetur modi sunt soluta, nulla quis tempora quo porro suscipit dicta enim corrupti error pariatur, ullam consequuntur nostrum. Aperiam consectetur maxime expedita dolore. Doloremque sequi non libero temporibus?
-                <h2 class="mt-2">¿Como surgió?</h2>
-                Expedita quas voluptatibus incidunt nam laborum officiis sed, tenetur cumque soluta, sunt recusandae eaque. Omnis libero eos eligendi ex minima obcaecati atque voluptatum beatae, rerum veniam repudiandae id pariatur eaque!
-                Minus, saepe repellendus. Architecto sit ullam repellat eos et ab, molestias officia facere aspernatur quia aut similique possimus, eius placeat voluptatem nihil. Laborum earum neque adipisci sit quasi facilis natus.
-            </div>
-  <div class="col-lg-8" id="carousel_div">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active slide1">
-                            <h2 class="line anim-typewriter">¿Como usar CloudCat? <i class="fas fa-paw"></i></h2>
-                        </div>
-                        <div class="carousel-item slide2">
-                            <div class="container mt-5">
-                                <h3 style="text-align:left; color:#000;">Crear cuenta CloudCat</h3>
-                                <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis magni fugiat minima voluptate similique dolorum exercitationem cum assumenda id qui ratione dolor reiciendis aperiam, laudantium optio, facilis quidem corporis? Tempore.</p><br>
-                                <h3 style="text-align:left; color:#000;">Iniciar Sesion</h3>
-                                <p class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis magni fugiat minima voluptate similique dolorum exercitationem cum assumenda id qui ratione dolor reiciendis aperiam, laudantium optio, facilis quidem corporis? Tempore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum debitis ullam dolore dolor blanditiis aut nihil, at earum nobis sapiente, fugit modi reiciendis eos iure. Dolore sed minima soluta magni.</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item slide">
-                            <div class="container mt-5">
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h2>Bienvenido a SuDocs</h2>
+					<p>Aqui encontraras documentos de soporte para desarrolladores</p>
+					<ul class="actions">
+						<li><a href="#content" class="button big special">Iniciar Sesi&oacute;n</a></li>
+						<li><a href="#elements" class="button big alt">Empezar ahora</a></li>
+					</ul>
+				</div>
+			</section>
 
-                            </div>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-            </div>
+		<!-- One -->
+			<section id="one" class="wrapper style1">
+				<header class="major">
+					<h2>Ipsum feugiat consequat</h2>
+					<p>Tempus adipiscing commodo ut aliquam blandit</p>
+				</header>
+				<div class="container">
+					<div class="row">
+						<div class="4u">
+							<section class="special box">
+								<i class="icon fa-area-chart major"></i>
+								<h3>Justo placerat</h3>
+								<p>Eu non col commodo accumsan ante mi. Commodo consectetur sed mi adipiscing accumsan ac nunc tincidunt lobortis.</p>
+							</section>
+						</div>
+						<div class="4u">
+							<section class="special box">
+								<i class="icon fa-refresh major"></i>
+								<h3>Blandit quis curae</h3>
+								<p>Eu non col commodo accumsan ante mi. Commodo consectetur sed mi adipiscing accumsan ac nunc tincidunt lobortis.</p>
+							</section>
+						</div>
+						<div class="4u">
+							<section class="special box">
+								<i class="icon fa-cog major"></i>
+								<h3>Amet sed accumsan</h3>
+								<p>Eu non col commodo accumsan ante mi. Commodo consectetur sed mi adipiscing accumsan ac nunc tincidunt lobortis.</p>
+							</section>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+		<!-- Two -->
+			<section id="two" class="wrapper style2">
+				<header class="major">
+					<h2>Commodo accumsan aliquam</h2>
+					<p>Amet nisi nunc lorem accumsan</p>
+				</header>
+				<div class="container">
+					<div class="row">
+						<div class="6u">
+							<section class="special">
+								<a href="#" class="image fit"><img src="images/pic01.jpg" alt="" /></a>
+								<h3>Mollis adipiscing nisl</h3>
+								<p>Eget mi ac magna cep lobortis faucibus accumsan enim lacinia adipiscing metus urna adipiscing cep commodo id. Ac quis arcu amet. Arcu nascetur lorem adipiscing non faucibus odio nullam arcu lobortis. Aliquet ante feugiat. Turpis aliquet ac posuere volutpat lorem arcu aliquam lorem.</p>
+								<ul class="actions">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+						</div>
+						<div class="6u">
+							<section class="special">
+								<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+								<h3>Neque ornare adipiscing</h3>
+								<p>Eget mi ac magna cep lobortis faucibus accumsan enim lacinia adipiscing metus urna adipiscing cep commodo id. Ac quis arcu amet. Arcu nascetur lorem adipiscing non faucibus odio nullam arcu lobortis. Aliquet ante feugiat. Turpis aliquet ac posuere volutpat lorem arcu aliquam lorem.</p>
+								<ul class="actions">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+						</div>
+					</div>
+				</div>
+			</section>
 
-        </div>
-    </div>
-  </div>
+		<!-- Three -->
+			<section id="three" class="wrapper style1">
+				<div class="container">
+					<div class="row">
+						<div class="8u">
+							<section>
+								<h2>Mollis ut adipiscing</h2>
+								<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
+								<p>Vis accumsan feugiat adipiscing nisl amet adipiscing accumsan blandit accumsan sapien blandit ac amet faucibus aliquet placerat commodo. Interdum ante aliquet commodo accumsan vis phasellus adipiscing. Ornare a in lacinia. Vestibulum accumsan ac metus massa tempor. Accumsan in lacinia ornare massa amet. Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing semper ornare pellentesque.</p>
+							</section>
+						</div>
+						<div class="4u">
+							<section>
+								<h3>Magna massa blandit</h3>
+								<p>Feugiat amet accumsan ante aliquet feugiat accumsan. Ante blandit accumsan eu amet tortor non lorem felis semper. Interdum adipiscing orci feugiat penatibus adipiscing col cubilia lorem ipsum dolor sit amet feugiat consequat.</p>
+								<ul class="actions">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+							<hr />
+							<section>
+								<h3>Ante sed commodo</h3>
+								<ul class="alt">
+									<li><a href="#">Erat blandit risus vis adipiscing</a></li>
+									<li><a href="#">Tempus ultricies faucibus amet</a></li>
+									<li><a href="#">Arcu commodo non adipiscing quis</a></li>
+									<li><a href="#">Accumsan vis lacinia semper</a></li>
+								</ul>
+							</section>
+						</div>
+					</div>
+				</div>
+			</section>			
+			
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<div class="row double">
+						<div class="6u">
+							<div class="row collapse-at-2">
+								<div class="6u">
+									<h3>Accumsan</h3>
+									<ul class="alt">
+										<li><a href="#">Nascetur nunc varius</a></li>
+										<li><a href="#">Vis faucibus sed tempor</a></li>
+										<li><a href="#">Massa amet lobortis vel</a></li>
+										<li><a href="#">Nascetur nunc varius</a></li>
+									</ul>
+								</div>
+								<div class="6u">
+									<h3>Faucibus</h3>
+									<ul class="alt">
+										<li><a href="#">Nascetur nunc varius</a></li>
+										<li><a href="#">Vis faucibus sed tempor</a></li>
+										<li><a href="#">Massa amet lobortis vel</a></li>
+										<li><a href="#">Nascetur nunc varius</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="6u">
+							<h2>Aliquam Interdum</h2>
+							<p>Blandit nunc tempor lobortis nunc non. Mi accumsan. Justo aliquet massa adipiscing cubilia eu accumsan id. Arcu accumsan faucibus vis ultricies adipiscing ornare ut. Mi accumsan justo aliquet.</p>
+							<ul class="icons">
+								<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+								<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+							</ul>
+						</div>
+					</div>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a></li>
+						<li>Images: <a href="http://unsplash.com">Unsplash</a></li>
+					</ul>
+				</div>
+			</footer>
   <script src="<?= base_url() ?>libs/js/jquery-3.4.1.min.js"></script>
-  <script src="<?= base_url(); ?>libs/js/popper.min.js"></script>
-    <script src="<?= base_url(); ?>libs/js/bootstrap.js"></script>
-    <script src="<?= base_url(); ?>libs/js/morph.js"></script>
-    <script src="<?= base_url(); ?>libs/js/tween.js"></script>
-  <!-- ----------------------------------Pezonalizado-------------------------------- -->
-  <script src="<?= base_url() ?>libs/js/homepage.js"></script>
+  <script src="<?= base_url() ?>libs/js/homepage/skel.min.js"></script>
+  <script src="<?= base_url() ?>libs/js/homepage/skel-layers.min.js"></script>
+  <script src="<?= base_url() ?>libs/js/homepage/init.js"></script>
+
+   
+  
 </body>
 </html>
