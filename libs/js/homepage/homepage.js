4f095e965a0eac5c12e65b009438c34d8d5cf367
Victoria Castro <victoria.akicastro92@gmail.com>
2019-10-09 15:08:40 -0600

    header homepage

--- libs/js/homepage/homepage.js
+++ libs/js/homepage/homepage.js
@@ -1,21 +1,50 @@
-$(function() {
-	var pull 		= $('#pull');
-	menu 		= $('nav ul');
-	menuHeight	= menu.height();
-    $(pull).on('click', function(e) {
-	    e.preventDefault();
-	    menu.slideToggle();
-    });
-    $(window).resize(function(){
-    		var w = $(window).width();
-    		if(w > 320 && menu.is(':hidden')) {
-    			menu.removeAttr('style');
-    		}
-      });
-      
-      $(".hover").mouseleave(
-        function () {
-          $(this).removeClass("hover");
-        }
-      );
-});
+consoleText(['¡Hola Mundo!', 'Este es Sudocs', 'Bienvenidos'], 'text',['#000000','#000000','#000000']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
