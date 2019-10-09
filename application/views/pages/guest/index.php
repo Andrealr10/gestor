570704cb819906dfed0d132e7a55b3375e45b0a2
Cesar Linares <celinares@hotmail.com>
2019-10-09 09:13:03 -0600

    cambio

--- application/views/pages/guest/index.php
+++ application/views/pages/guest/index.php
@@ -41,7 +41,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="<?= base_url('guest') ?>" class="brand-link">
+      <a data-widget="pushmenu" href="#" class="brand-link">
         <img src="<?= base_url() ?>home/images/subcategorias/logo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Nombre</span>
       </a>
