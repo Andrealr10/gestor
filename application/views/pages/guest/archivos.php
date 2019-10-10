284f03e4b4f6d5e704c0b8c1ab188e4c9fc78fa4
Eduardo Hernández <no>
2019-10-10 16:51:37 -0600

    otro commit

--- application/views/pages/guest/archivos.php
+++ application/views/pages/guest/archivos.php
@@ -30,7 +30,7 @@
                 <div data-toggle="modal" data-target="#share<?= $archivo->id_archivo ?>">
                     <div class="overlay"></div>
                     <div class="iconito">
-                        <i class="fas fa-share-alt fa-3x" style="color:white;"></i>
+                        <i class="fas fa-qrcode fa-3x" style="color:white;"></i>
                     </div>
                 </div>
             </div>
