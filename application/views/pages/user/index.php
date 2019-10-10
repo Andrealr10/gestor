3bb7c1f9a5741303942490c969dc1425cdcc66be
Eduardo Hernández <no>
2019-10-10 13:58:16 -0600

    otro cambio

--- application/views/pages/user/index.php
+++ application/views/pages/user/index.php
@@ -36,9 +36,6 @@
 
       <!-- Right navbar links -->
       <ul class="navbar-nav ml-auto">
-        <button class="btn_salir">
-          <li><a href="<?= base_url('login') ?>" class="salir">Cerrar Sesión&nbsp<i class="fas fa-sign-out-alt"></i></a></li>
-        </button>
         <button class="btn_salir"><li><a href="<?= base_url('login') ?>/logout" class="iniciar">Cerrar Sesión&nbsp<i class="fas fa-sign-out-alt"></i></a></li></button>
       </ul>
     </nav>
