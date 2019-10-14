5431eefbe93574937e0dca8837a60c9456d808dc
Andrea Linares <andrealinaresr96@gmail.com>
2019-10-13 19:38:46 -0600

    cambioss

--- application/views/pages/user/archivos.php
+++ application/views/pages/user/archivos.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="<?= base_url(); ?>libs/css/contenido.css">
+<link rel="stylesheet" href="<?= base_url(); ?>libs/css/subir.css">
 <div class="row">
     <input type="text" name="subcategoria" id="subcategoria" hidden value="<?= $subcategoria->id_subcategoria ?>">
     <input type="text" name="categoria" id="categoria" hidden value="<?= $categoria->nombre ?>">
@@ -8,10 +9,12 @@
 
             <!-- Our markup, the important part here! -->
             <div id="drag-and-drop-zone" class="dm-uploader p-5">
-                <h3 class="mb-5 mt-5 text-muted">Drag &amp; drop files here</h3>
-
-                <div class="btn btn-primary btn-block mb-5">
-                    <span>Open the file Browser</span>
+                <!-- <h3 class="mb-5 mt-5 text-muted">Drag &amp; drop files here</h3> -->
+                <h3 class="mb-5 mt-5 ">Arrastra los archivos aqu&iacute;</h3><br><br>
+                
+                <div class="btn btn-subir btn-block mb-5">
+                    <!-- <span>Open the file Browser</span> -->
+                    <span>Seleccionar el archivo</span>
                     <input type="file" title='Click to add Files' />
                 </div>
             </div><!-- /uploader -->
@@ -20,11 +23,11 @@
         <div class="col-md-6 col-sm-12">
             <div class="card h-100">
                 <div class="card-header">
-                    File List
+                    Lista de Archivos
                 </div>
 
                 <ul class="list-unstyled p-2 d-flex flex-column col" id="files">
-                    <li class="text-muted text-center empty">No files uploaded.</li>
+                    <li class="text-muted text-center empty">No hay archivos cargados</li>
                 </ul>
             </div>
         </div>
