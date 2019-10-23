9f34421273aefdb6b829fc05adf18456ef3ecd34
Victoria Castro <victoria.akicastro92@gmail.com>
2019-10-23 14:09:58 -0600

    cambio de vista reset.php

--- application/controllers/HomePage.php
+++ application/controllers/HomePage.php
@@ -11,11 +11,8 @@ class HomePage extends CI_Controller {
 
 	public function index()
 	{
-		// $data = ['categorias' => $this->categoriaModel->getAll()];
-		// $this->load->library('javascript'); esto ya no tiene soporte
-		// $this->load->helper('url'); // Se utiliza un helper para manejo de url
-		// $this->load->view('includes/navUser',$data); // Menu
+
 		$this->load->view('homepage'); 
-		// $this->load->view('includes/foot');
+		
 	}
 }
